ea84b59ca2f9028dea345daad55ed428bc3f2a7f
Professor <Professor@LAB4PROF>
2014-08-22 20:07:36 -0300

    Entrega do Exercicio 1 == Alteracao ...

--- src/e1/AlexandreAguado.java
+++ src/e1/AlexandreAguado.java
@@ -17,6 +17,7 @@ public class AlexandreAguado {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Olaaaa minha gente!!!");
     }
     
 }
